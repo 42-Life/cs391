b98e19d85abc00bcedfb8a7c80d3bef9fefcfd92
Ajani Hickling <ajanih@bu.edu>
2025-02-20 01:56:40 -0500

    Added basic configuration for MP2 React Application

---
+++ mini-projects/mp-2/src/App.tsx
@@ -0,0 +1,10 @@
+/* importing necessary dependencies & packages */
+import styled from "styled-components";
+import {useState, useEffect} from "react";
+
+export default function App() {
+    return (
+        <>
+        </>
+    );
+}
