3830a3b42b6ab2308fa896d7606d6949569ece6c
Ajani Hickling <ajanih@bu.edu>
2025-03-07 23:57:51 -0500

    fixed h4 in mobile

--- mini-projects/mp-3/src/components/pages/Projects.tsx
+++ mini-projects/mp-3/src/components/pages/Projects.tsx
@@ -2,7 +2,7 @@ import Header from "../primary-tags/Header.tsx";
 import Nav from "../primary-tags/Nav.tsx";
 import Footer from "../primary-tags/Footer.tsx";
 import {NewMain} from "../primary-tags/NewMain.tsx";
-import {PageText, PageTitle} from "../ui/StyledText.tsx";
+import {PageText, PageTitle, SmallHeading} from "../ui/StyledText.tsx";
 import {StyledWrapper, TwoColumnLayout} from "../ui/StyledDivs.tsx";
 import Calculator from "../calculator/Calculator.tsx";
 
@@ -17,7 +17,7 @@ export default function Projects() {
                     <PageText>A functional JavaScript calculator can be found on this page.
                         It has been designed to resemble a face. Please try it out!</PageText>
 
-                    <h4>Please enter two numbers, and select an operation</h4>
+                    <SmallHeading>Please enter two numbers, and select an operation</SmallHeading>
                     <Calculator/>
                 </NewMain>
             </TwoColumnLayout>
